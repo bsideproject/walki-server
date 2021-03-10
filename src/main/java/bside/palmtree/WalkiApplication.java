77857b0cded4224628c96fd2307ca66ed0588d96
yunheehyeon <ok94life@daum.net>
2021-03-11 00:34:10 +0900

    init project

---
+++ src/main/java/bside/palmtree/WalkiApplication.java
@@ -0,0 +1,16 @@
+package bside.palmtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by YHH on 2021/03/11
+ */
+@SpringBootApplication
+public class WalkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkiApplication.class, args);
+	}
+
+}
