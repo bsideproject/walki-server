b9b04e8e7007106a5681c94d570b7e1ac6707d80
yunheehyeon <ok94life@daum.net>
2021-04-08 22:25:01 +0900

    #4 Report
    유저 월 리포트 조회

---
+++ src/main/java/bside/palmtree/domain/challenge/report/Report.java
@@ -0,0 +1,40 @@
+package bside.palmtree.domain.challenge.report;
+
+import java.util.List;
+
+import bside.palmtree.domain.challenge.Challenge;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by YHH on 2021/04/05
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Report {
+	private Integer stepAchievement;
+	private Integer stepGoal;
+	private Integer challengeAchievement;
+	private Integer challengeGoal;
+	private List<Challenge> challenges;
+
+	public Report(List<Challenge> challenges) {
+		this.challenges = challenges;
+		this.stepAchievement = 0;
+		this.stepGoal = 0;
+		this.challengeAchievement = 0;
+		this.challengeGoal = challenges.size();
+
+		for (Challenge challenge : challenges) {
+			if (challenge.isSuccess()) {
+				this.challengeAchievement++;
+			}
+			this.stepAchievement += challenge.getStep();
+			this.stepGoal += challenge.getStepGoal();
+		}
+	}
+}
