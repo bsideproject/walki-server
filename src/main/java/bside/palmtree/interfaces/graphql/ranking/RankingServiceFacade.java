8c65c1578be135e551b2282cd765e29a940f76cc
yunheehyeon <ok94life@daum.net>
2021-10-31 21:56:24 +0900

    fix : 랭킹 맴버 추가

--- src/main/java/bside/palmtree/interfaces/graphql/ranking/RankingServiceFacade.java
+++ src/main/java/bside/palmtree/interfaces/graphql/ranking/RankingServiceFacade.java
@@ -2,6 +2,7 @@ package bside.palmtree.interfaces.graphql.ranking;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
@@ -10,7 +11,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import bside.palmtree.config.LoggedIn;
+import bside.palmtree.domain.membercoach.MemberCoach;
+import bside.palmtree.interfaces.graphql.coach.Coach;
 import bside.palmtree.interfaces.graphql.member.Member;
+import bside.palmtree.service.membercoach.MemberCoachService;
 import bside.palmtree.service.ranking.RankingService;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
@@ -26,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 @GraphQLApi
 public class RankingServiceFacade {
 	private final RankingService rankingService;
+	private final MemberCoachService memberCoachService;
 	private final ModelMapper modelMapper;
 
 	@GraphQLMutation(name = "createRankings")
@@ -42,7 +47,7 @@ public class RankingServiceFacade {
 
 		return this.rankingService.findTop10ByDate(date).stream()
 			.map(ranking -> Ranking.builder()
-				.member(this.modelMapper.map(ranking.getMember(), Member.class))
+				.member(convertMember(ranking.getMember()))
 				.number(ranking.getNumber())
 				.challengeDate(ranking.getChallengeDate())
 				.build())
@@ -57,10 +62,22 @@ public class RankingServiceFacade {
 
 		return this.rankingService.findAllByMemberAndDate(member, start, end).stream()
 			.map(ranking -> Ranking.builder()
-				.member(this.modelMapper.map(ranking.getMember(), Member.class))
+				.member(convertMember(ranking.getMember()))
 				.number(ranking.getNumber())
 				.challengeDate(ranking.getChallengeDate())
 				.build())
 			.collect(Collectors.toList());
+	}
+
+	private Member convertMember(bside.palmtree.domain.member.Member member) {
+		MemberCoach memberCoach = this.memberCoachService.findBy(member);
+
+		Member memberDto = this.modelMapper.map(member, Member.class);
+
+		if (Objects.nonNull(memberCoach)) {
+			memberDto.setCoach(this.modelMapper.map(memberCoach.getCoach(), Coach.class));
+		}
+
+		return memberDto;
 	}
 }
