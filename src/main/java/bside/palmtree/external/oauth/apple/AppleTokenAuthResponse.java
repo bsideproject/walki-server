efb3e451fef7a241bedc303b7530110f00e2612b
yunheehyeon <ok94life@daum.net>
2021-11-14 16:12:52 +0900

    fix: 애플 로그인 수정

--- src/main/java/bside/palmtree/external/oauth/apple/AppleTokenAuthResponse.java
+++ src/main/java/bside/palmtree/external/oauth/apple/AppleTokenAuthResponse.java
@@ -1,14 +1,23 @@
 package bside.palmtree.external.oauth.apple;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 public class AppleTokenAuthResponse {
+	@JsonProperty(value = "access_token")
 	private String accessToken;
+	@JsonProperty(value = "expires_in")
 	private Long expiresIn;
+	@JsonProperty(value = "id_token")
 	private String idToken;
+	@JsonProperty(value = "refresh_token")
 	private String refreshToken;
+	@JsonProperty(value = "token_type")
 	private String tokenType;
 }
